754b4f806b0067c6fb6b6731fe27c77a6e2c9127
AnderZhu <92746632+AnderZhu@users.noreply.github.com>
2024-06-18 17:09:03 +0800

    Update merge_quality_assessment.py

--- M1_Retinal_Image_quality_EyePACS/merge_quality_assessment.py
+++ M1_Retinal_Image_quality_EyePACS/merge_quality_assessment.py
@@ -6,8 +6,11 @@ import os
 
 result_Eyepacs = './test_outside/results_ensemble.csv'
 
-if not os.path.exists('../Results/M1/Good_quality/'):
-    os.makedirs('../Results/M1/Good_quality/')
+assert(os.path.exists('/content/drive/My Drive/'))
+
+PATH = os.path.join('/content/drive/My Drive/', '/Results/M1/Good_quality/'
+if not os.path.exists(PATH):
+    os.makedirs(PATH)
 # if not os.path.exists('../Results/M1/Bad_quality/'):
 #     os.makedirs('../Results/M1/Bad_quality/')
 
@@ -25,11 +28,11 @@ for i in range(len(name_list)):
     
     if Eyepacs_pre[i]==0:
         Eye_good+=1
-        shutil.copy(name_list[i], '../Results/M1/Good_quality/')
+        shutil.copy(name_list[i], PATH)
     elif (Eyepacs_pre[i]==1) and (Eyepacs_bad_mean[i]<0.25):
     #elif (Eyepacs_pre[i]==1) and (Eyepacs_bad_mean[i]<0.25) and (Eyepacs_usable_sd[i]<0.1):
         Eye_good+=1
-        shutil.copy(name_list[i], '../Results/M1/Good_quality/')        
+        shutil.copy(name_list[i], PATH)        
     else:
         Eye_bad+=1        
         # shutil.copy(name_list[i], '../Results/M1/Bad_quality/')
