ea8da61f8892dab3060df84996c56e3a8ccb1308
AnderZhu <92746632+AnderZhu@users.noreply.github.com>
2024-06-20 18:24:58 +0800

    Update test_outside.sh

--- M1_Retinal_Image_quality_EyePACS/test_outside.sh
+++ M1_Retinal_Image_quality_EyePACS/test_outside.sh
@@ -9,7 +9,7 @@ do
     do
     seed_number=$((42-2*n_round))
     CUDA_VISIBLE_DEVICES=${CUDA_NUMBER} python test_outside.py --e=1 --b=64 --task_name='Retinal_quality' --model=${model} --round=${n_round} --train_on_dataset='EyePACS_quality' \
-    --test_on_dataset='customised_data' --test_csv_dir='../Results/M0/images/' --n_class=3 --seed_num=${seed_number}
+    --test_on_dataset='customised_data' --test_csv_dir='/content/drive/My Drive/RFI-80GB/Results/M0/images/' --n_class=3 --seed_num=${seed_number}
 
     
     done
