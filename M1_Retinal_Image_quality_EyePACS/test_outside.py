2769ef0103f25953fc8d23810a6cf57b0b6741a1
AnderZhu <92746632+AnderZhu@users.noreply.github.com>
2024-06-20 18:54:18 +0800

    Update test_outside.py

--- M1_Retinal_Image_quality_EyePACS/test_outside.py
+++ M1_Retinal_Image_quality_EyePACS/test_outside.py
@@ -148,6 +148,8 @@ def test_net(model_fl_1,
                     pbar.update(imgs.shape[0])
         
     Data4stage2 = pd.DataFrame({'Name':filename_list, 'softmax_good':np.array(prediction_list_mean)[:,0],'softmax_usable':np.array(prediction_list_mean)[:,1],'softmax_bad':np.array(prediction_list_mean)[:,2], 'good_sd':np.array(prediction_list_std)[:,0],'usable_sd':np.array(prediction_list_std)[:,1],'bad_sd':np.array(prediction_list_std)[:,2], 'Prediction': prediction_decode_list})
+    if not os.path.exists('./test_outside'):
+        os.makedirs('./test_outside')
     Data4stage2.to_csv('./test_outside/results_ensemble.csv', index = None, encoding='utf8')
     if not os.path.exists(os.path.join(PATH, '/Results/M1')):
         os.makedirs(os.path.join(PATH, '/Results/M1'))
