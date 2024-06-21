009d400848e6069e44e6aa1e6694a2317f1ee0dc
AnderZhu <92746632+AnderZhu@users.noreply.github.com>
2024-06-21 09:04:22 +0800

    Update EyeQ_process_main.py

--- M0_Preprocess/EyeQ_process_main.py
+++ M0_Preprocess/EyeQ_process_main.py
@@ -25,12 +25,12 @@ def process(train_list, save_path):
             print('continue...')
             continue
         try:
-            resolution_ = 1 # resolution_list['res'][resolution_list['fundus']==image_path].values[0]
-            list_resolution.append(resolution_)
+            # resolution_ = 1 # resolution_list['res'][resolution_list['fundus']==image_path].values[0]
+            # list_resolution.append(resolution_)
             img = prep.imread(dst_image)
             r_img, borders, mask, r_img, radius_list,centre_list_w, centre_list_h = prep.process_without_gb(img,img,radius_list,centre_list_w, centre_list_h)
             prep.imwrite(save_path + image_path.split('.')[0] + '.png', r_img)
-            name_list.append(image_path.split('.')[0] + '.png')
+            # name_list.append(image_path.split('.')[0] + '.png')
         
         except Exception as e:
             print(e)
@@ -56,10 +56,10 @@ def process(train_list, save_path):
     #         exit(1)
     #         pass
 
-    scale_list = [a*2/912 for a in radius_list]
-    scale_resolution = [a*b*1000 for a,b in zip(list_resolution,scale_list)]
-    Data4stage2 = pd.DataFrame({'Name':name_list, 'centre_w':centre_list_w, 'centre_h':centre_list_h, 'radius':radius_list, 'Scale':scale_list, 'Scale_resolution':scale_resolution})
-    Data4stage2.to_csv(os.path.join(PATH, 'Results/M0/crop_info.csv'), index = None, encoding='utf8')
+    # scale_list = [a*2/912 for a in radius_list]
+    # scale_resolution = [a*b*1000 for a,b in zip(list_resolution,scale_list)]
+    # Data4stage2 = pd.DataFrame({'Name':name_list, 'centre_w':centre_list_w, 'centre_h':centre_list_h, 'radius':radius_list, 'Scale':scale_list, 'Scale_resolution':scale_resolution})
+    # Data4stage2.to_csv(os.path.join(PATH, 'Results/M0/crop_info.csv'), index = None, encoding='utf8')
 
 
 if __name__ == "__main__":
